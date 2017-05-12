e4b39ce0e913c3a06396814144fe743fa6a9ba6d
Albert <castelherreraalbert@gmail.com>
2017-05-12 03:46:36 +0200

    Added Menu Hello
    fef

--- wp-content/plugins/pluginAlbert/pluginalbert.php
+++ wp-content/plugins/pluginAlbert/pluginalbert.php
@@ -10,5 +10,11 @@ Author URI: ...
 
 
 register_activation_hook( __FILE__, 'pluginprefix_function_to_run' );
-
+add_action("admin_menu","addMenu");
+function addMenu(){
+  add_menu_page("Example Page","First Page",4,"First Page","Albert Plugin");
+}
+function exampleMenu(){
+  echo "HelloWorld";
+}
 ?>
